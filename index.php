d29510973d386a4e2ae2971566ae65237f2f51a1
Benoît <benoit.catillon@gmail.com>
2017-02-07 21:05:33 +0100

    Création du fichier index.php - Dernier commit pour l'exercice Git & Github de Marc Gauthier

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+
+      <meta charset="UTF-8">
+      <meta name="description" content="Exercice Git & Github - OpenClassRooms">
+      <meta name="author" content="Benoît Catillon">
+
+      <title>Exercice Git & Github - OpenClassRooms</title>
+
+    </head>
+
+    <body>
+
+      <?php include 'content.inc.php'; ?>
+
+    </body>
+
+</html>
