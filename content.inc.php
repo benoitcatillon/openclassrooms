8097469b6e2111ef59d50ab5b5b03eaa60ee604a
Benoît <benoit.catillon@gmail.com>
2017-02-07 21:23:50 +0100

    Ajout de remerciements à l'attention des correcteurs. VRAI 4ème et dernier commit !

--- content.inc.php
+++ content.inc.php
@@ -3,5 +3,7 @@
           sur OpenClassRooms.\n\n
           J'apprends Git dans le but de me familiariser avec l'outil afin
           d'être opérationnel en entreprise, lorsque j'aurais obtenu mon
-          diplôme de Concepteur Développeur Web.";
+          diplôme de Concepteur Développeur Web.\n\n
+          J'espère que cette relecture n'aura pas été trop longue ;)\n
+          Merci à toi, Correcteur !";
 ?>
