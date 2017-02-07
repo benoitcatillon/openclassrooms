6c0cfebecae6d3700edc99ad5630fcfcb2b4710c
Benoît <benoit.catillon@gmail.com>
2017-02-07 20:57:29 +0100

    Création du fichier content.inc.php - Ce fichier sera intégré à l'index

---
+++ content.inc.php
@@ -0,0 +1,7 @@
+<?php
+    echo "Hello World ! Ce fichier à pour but de présenter mon exercice
+          sur OpenClassRooms.\n\n
+          J'apprends Git dans le but de me familiariser avec l'outil afin
+          d'être opérationnel en entreprise, lorsque j'aurais obtenu mon
+          diplôme de Concepteur Développeur Web.";
+?>
